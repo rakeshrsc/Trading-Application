858f43a733d1e91a0d9428f5c7698751edcbb7a9
Rakesh Rajendran <rakeshrsc.91@gmail.com>
2023-05-07 16:21:12 +0200

    feature #3 : Modified in Application.java file 1.Added commonBehaviour method, which helps to keep all the signals data 2.modified in handleSignal method to call commonBehaviour method and invoke the methods

--- src/main/java/com/Trading/com/base/Application.java
+++ src/main/java/com/Trading/com/base/Application.java
@@ -1,35 +1,73 @@
 package com.Trading.com.base;
 
+
+import java.util.*;
+
 class Application implements SignalHandler {
-    public void handleSignal(int signal) {
+
+       private static Map<Integer, Runnable> methodMap = new HashMap<>();
+
+    /**
+     * Add All respective signals in commonBehaviour method
+     * @param sigVal
+     * @throws NoSuchMethodException
+     */
+    public void commonBehaviour(int sigVal) throws NoSuchMethodException {
         Algo algo = new Algo();
 
-        switch (signal) {
-            case 1:
+        if (sigVal == 1) {
+            methodMap.put(1, () -> {
                 algo.setUp();
-                algo.setAlgoParam(1,60);
+                algo.setAlgoParam(1, 60);
                 algo.performCalc();
                 algo.submitToMarket();
-                break;
-
-            case 2:
+            });
+        } else if (sigVal == 2) {
+            methodMap.put(2, () -> {
                 algo.reverse();
-                algo.setAlgoParam(1,80);
+                algo.setAlgoParam(1, 80);
                 algo.submitToMarket();
-                break;
-
-            case 3:
-                algo.setAlgoParam(1,90);
-                algo.setAlgoParam(2,15);
+            });
+        } else if (sigVal == 3) {
+            methodMap.put(3, () -> {
+                algo.setAlgoParam(1, 90);
+                algo.setAlgoParam(2, 15);
                 algo.performCalc();
                 algo.submitToMarket();
-                break;
-
-            default:
+            });
+        } else {
+            methodMap.put(51, () -> {
                 algo.cancelTrades();
-                break;
+            });
         }
 
-        algo.doAlgo();
+        //This method invoke all cases
+        methodMap.put(100, () -> {
+            algo.doAlgo();
+        });
+    }
+
+    /**
+     * Method handleSignal Override from SignalHandler Interface
+     * @param signal
+     */
+    @Override
+    public void handleSignal(int signal) {
+
+        try {
+            if (signal <0 || signal>50) {
+                commonBehaviour(51);
+                methodMap.get(51).run();
+                methodMap.get(100).run();
+            }else {
+                commonBehaviour(signal);
+                methodMap.get(signal).run();
+                methodMap.get(100).run();
+            }
+        } catch (NoSuchMethodException a) {
+            System.out.println(a.getMessage());
+        }finally {
+            methodMap.clear();
+        }
     }
 }
